38ac509d408965a81e56c2057378d624902ab64f
mcolomba <marc057>
2022-10-24 12:51:19 +0200

    Added memchr and memcmp, this last one uncompleted

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,22 +6,23 @@
 /*   By: mcolomba <mcolomba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 14:19:17 by mcolomba          #+#    #+#             */
-/*   Updated: 2022/09/30 11:51:31 by mcolomba         ###   ########.fr       */
+/*   Updated: 2022/10/24 12:50:28 by mcolomba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memchr(const void *s, int c, unsigned int n)
 {
-	void			*pos;
+	const char		*pos;
 	unsigned int	i;
 
-	pos = s;
+	pos = (const char *) s;
 	i = 0;
-	while (pos[i])
+	while (i < n)
 	{
-		if (*pos == (char) c)
-			return (pos);
+		if (pos[i] == c)
+			return ((void *) &pos[i]);
 		pos++;
+		i++;
 	}
-	return (pos);
+	return (0);
 }
--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,27 @@
 /*   By: mcolomba <mcolomba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 14:19:21 by mcolomba          #+#    #+#             */
-/*   Updated: 2022/09/23 14:44:58 by mcolomba         ###   ########.fr       */
+/*   Updated: 2022/10/24 12:50:08 by mcolomba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_memcmp(const void *s1, const void *s2, unsigned int n)
+{
+	const unsigned char	*ptr1;
+	const unsigned char	*ptr2;
+	unsigned int		i;
+
+	if (n != 0)
+	{
+		ptr1 = (const unsigned char *) s1;
+		ptr2 = (const unsigned char *) s2;
+
+		i = 0;
+		while (i < n)
+		{
+			i++;
+			// TODO: Finish
+		}
+	}
+	return (0);
+}
